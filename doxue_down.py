c3a47cfeed642dc13fc65ed1d386427f6b47f9c8
Tao LI <litao.91@bytedance.com>
2020-06-11 15:04:35 +0800

    Working version
    Change-Id: I369f20a0a48e0b41233dfe0446932808da90ce10

--- doxue_down.py
+++ doxue_down.py
@@ -2,23 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+import sys
+import os
 
-
-
-def download(driver, url):
+def download(driver, url, base):
     driver.get(url)
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 50).until(
       expected_conditions.presence_of_element_located(
         (By.TAG_NAME, 'source')
       )
     )
     video_src = driver.find_element_by_tag_name("source").get_attribute("src")
-    name = driver.find_element_by_tag_name("h4").get_attribute("innerHTML")
+    name = driver.find_element_by_tag_name("h4").get_attribute("innerHTML").strip()
     print(name + " = " + video_src)
-
+    cmd = '/Users/taoli/.local/bin/m3u8 -u="%s" -o="%s"' % (video_src, 'downloads/%s/%s' % (base, name))
+    print('running: ' + cmd)
+    os.system(cmd)
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
-    url = "https://www.doxue.com/video/play/1704"
-    download(driver, url)
+    url_pattern = "https://www.doxue.com/video/play/%d"
+    for i in range(int(sys.argv[1]), int(sys.argv[2])):
+        try:
+            print('i = %d' %(i))
+            download(driver, url_pattern % i, sys.argv[3])
+        except:
+            pass
+
+    driver.close()
 
