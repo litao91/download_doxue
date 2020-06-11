2a915a84470bf0a984b9c94811179a3e92a3510d
Tao LI <litao.91@bytedance.com>
2020-06-11 15:52:35 +0800

    Extract page from page
    Change-Id: Ieb601ba75b1f308a02f811f5c7f29b47ced1a382

--- doxue_down.py
+++ doxue_down.py
@@ -2,8 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+import selenium.webdriver.common.by
 import sys
 import os
+
+def extract_urls(driver, page_url):
+    driver.get(url)
+    WebDriverWait(driver, 50).until(
+      expected_conditions.presence_of_element_located(
+        (By.ID, 'detailed-catalog')
+      )
+    )
+    catalog = driver.find_element_by_id('detailed-catalog')
+    hrefs = catalog.find_elements(By.TAG_NAME, "a")
+    return [href.get_attribute('href') for href in hrefs]
 
 def download(driver, url, base):
     driver.get(url)
@@ -15,10 +27,26 @@ def download(driver, url, base):
     video_src = driver.find_element_by_tag_name("source").get_attribute("src")
     name = driver.find_element_by_tag_name("h4").get_attribute("innerHTML").strip()
     print(name + " = " + video_src)
+    dest = 'downloads/%s/%s' % (base, name)
+    if os.path.exists(dest):
+        print(dest + ' exists, skip')
+        return
     cmd = '/Users/taoli/.local/bin/m3u8 -u="%s" -o="%s"' % (video_src, 'downloads/%s/%s' % (base, name))
     print('running: ' + cmd)
     os.system(cmd)
 
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    try:
+        url = sys.argv[1]
+        print('working on ' + url)
+        urls = extract_urls(driver, url)
+        for url in urls:
+            print(url)
+            download(driver, url, sys.argv[2])
+    finally:
+        driver.close()
+"""
 if __name__ == "__main__":
     driver = webdriver.Chrome()
     url_pattern = "https://www.doxue.com/video/play/%d"
@@ -30,4 +58,4 @@ if __name__ == "__main__":
             pass
 
     driver.close()
-
+"""
